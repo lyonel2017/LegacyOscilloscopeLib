5cd2b03d2b864b0a484df26b4e80bb7723a138fa
Lionel Blatter <lionel.blatter@wanadoo.fr>
2021-07-13 22:40:54 +0200

    Update public header

--- include/hp54600b.h
+++ include/hp54600b.h
@@ -4,27 +4,22 @@
 /* RS232 */
 int RS232_OpenComport();
 void RS232_CloseComport(int);
-int RS232_IsDCDEnabled(int);
-int RS232_IsRINGEnabled(int);
-int RS232_IsCTSEnabled(int);
-int RS232_IsDSREnabled(int);
-int RS232_enableDTR(int);
-int RS232_disableDTR(int);
-int RS232_enableRTS(int);
-int RS232_disableRTS(int);
-void RS232_flushRX(int);
-void RS232_flushTX(int);
-void RS232_flushRXTX(int);
 
-/* Utils for communication*/
+/* Utils for communication */
 int send(int, unsigned char*);
 int wsend(int, unsigned char*);
 int read_data(int, int, char*);
 int bsend(int, unsigned char*);
 int char_to_int(char);
-int read_data_block(int, char*);
+int rea_data_block(int, char*);
 
-/* Waveform*/
+/* IEEE 488.2 Common command */
+int set_RST(int cport_nr);
+int set_CLS(int cport_nr);
+int set_ESE(int cport_nr, int bit);
+int set_SRE(int cport_nr, int bit);
+
+/* Waveform command */
 int get_waveform_data(int, char*);
 int set_waveform_source(int, int);
 int set_waveform_format(int);
